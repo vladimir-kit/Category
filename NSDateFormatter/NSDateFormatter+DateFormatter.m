129950bb48c761216f251c108f9bc5be94df793f
vladimir-kit <jsdfjn@mail.ru>
2014-03-04 23:40:15 +0200

    Update NSDateFormatter+DateFormatter.m

--- NSDateFormatter/NSDateFormatter+DateFormatter.m
+++ NSDateFormatter/NSDateFormatter+DateFormatter.m
@@ -90,4 +90,12 @@ NSString *stringTodayMounthAndDay = nil;
     [self checkYear];
 }
 
+#pragma mark - 
++ (NSString *)todayDateTime {
+    NSDate *date = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss"];
+    return [dateFormatter stringFromDate:date];
+}
+
 @end
