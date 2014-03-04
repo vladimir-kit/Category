079bbf121d48f51b4a7d23355514cf56f46b5c66
vladimir-kit <jsdfjn@mail.ru>
2014-03-04 23:40:32 +0200

    Update NSDateFormatter+DateFormatter.h

--- NSDateFormatter/NSDateFormatter+DateFormatter.h
+++ NSDateFormatter/NSDateFormatter+DateFormatter.h
@@ -5,4 +5,6 @@
 /** По году/месяцу/дню пользователя например:(1991-12-29) высчитывает количество лет. */
 + (NSString *)getNumberOfYearsSetBirthdayDate:(NSString *)birthday;
 
++ (NSString *)todayDateTime;
+
 @end
